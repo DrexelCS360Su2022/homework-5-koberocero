85553e5eb636a24f140abe338ebf799dcddf9876
kmr454@drexel.edu <75811489+krocero1@users.noreply.github.com>
2022-08-05 15:25:33 -0400

    Homework 5 - Submission 1

--- HaskellIntro.hs
+++ HaskellIntro.hs
@@ -1,8 +1,11 @@
 {-# OPTIONS_GHC -fwarn-tabs #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module HaskellIntro where
 
 import Set
+import System.Win32 (COORD(x))
+import Data.Tree (flatten)
 
 -- Load this file into GHCi (say, with `ghci HaskellIntro.hs`) and type
 -- `isThisWorking` at the prompt. GHCi will tell you whether it's working!
@@ -15,41 +18,65 @@ isThisWorking = "Yes"
 --
 
 lastDigit :: Integer -> Integer
-lastDigit = error "lastDigit not yet defined"
+lastDigit num = num `rem` 10
 
 dropLastDigit :: Integer -> Integer
-dropLastDigit = error "dropLastDigit not yet defined"
+dropLastDigit num = num `div` 10
 
 toDigits :: Integer -> [Integer]
-toDigits = error "toDigits not yet defined"
+toDigits x = reverse(toDigitsHelper x)
+
+toDigitsHelper :: Integer -> [Integer]
+toDigitsHelper x | x <= 0 = []
+toDigitsHelper x = lastDigit x : toDigitsHelper (dropLastDigit x)
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = error "doubleEveryOther not yet defined"
+doubleEveryOther x = reverse (doubleEveryOtherHelper (reverse x))
+
+doubleEveryOtherHelper :: [Integer] -> [Integer]
+doubleEveryOtherHelper [] = []
+doubleEveryOtherHelper [x] = [x]
+doubleEveryOtherHelper (x:xs:ys) = x : xs*2 : doubleEveryOtherHelper ys
 
 sumDigits :: [Integer] -> Integer
-sumDigits = error "sumDigits not yet defined"
+sumDigits [] = 0
+sumDigits x = sumDigitList(head x) + sumDigits(drop 1 x)
+
+sumDigitList :: Integer -> Integer
+sumDigitList x = sum(toDigits x)
 
 validate :: Integer -> Bool
-validate = error "validate not yet defined"
+validate x = sumDigits (doubleEveryOther (toDigits x)) `mod` 10 == 0
 
 --
--- Problem 2
+-- Problem 2 
 --
 
 pow :: (a -> a) -> Int -> a -> a
-pow = error "pow not yet defined"
+pow f 0 = id
+pow f 1 = f
+pow f n = f . pow f (n-1)
+
 
 g :: Integer -> Integer
-g = error "g not yet defined"
+g 0 = 0
+g n = n - pow g 2 (n-1)
 
 h :: Integer -> Integer
-h = error "h not yet defined"
+h 0 = 0
+h n = n - pow h 3 (n-1)
 
 d :: Int -> Integer -> Integer
-d = error "d not yet defined"
+d i 0 = 0
+d i n = n - pow (d i) i (n-1)
+
 
 --
 -- Problem 3
 --
 
-powerSet = error "powerSet not yet defined"
+powerSet set    | isEmpty set = singleton empty
+                | otherwise = union (mapSet (insert x) (powerSet t)) (powerSet t)
+                 where t = snd (split set)
+                       x = fst (split set)
+
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # CS 360 Homework 3
 
-Name:  
+Name: Kobe Rocero
 
-Drexel Username:  
+Drexel Username: kmr454 
 
 Put anything else you want us to know here.
 
@@ -10,8 +10,13 @@ Put anything else you want us to know here.
 
 Please let us know how much time you spent on each problem. You may enter time using any format described [here](https://github.com/wroberts/pytimeparse). Please put your time estimates *on the same line* as the problem label, not on the following line.
 
-Problem 1:  
+Problem 1: 20 mins
 
-Problem 2:  
+This problem was fairly simple. I already knew how to do this problem in another language such as python or java. However the trick was converting the syntax over. After I got a grasp of syntax, the following problems took less time.
 
-Problem 3:  
+Problem 2: 20 mins
+
+I had little trouble with the first two parts of this problem. With part one, it was just basic recursion and base cases. The second part, I just had to do function g and then I could easily implement function h. Function g was a little troublesome, mainly because I wasn't sure if I could call g recursively through pow or not. Lastly the d function was more difficult. At first, I couldn't wrap my brain around what it was trying to do. I just needed some clarification and then I could solve it. 
+
+Problem 3: 30 mins
+This problem was the most challenging out of the three. First of all, I had to know what a power set of a set was. Then, I had to look at the slides to see the algorithm on constructing the power set itself. I then had to convert that psuedo code into haskell, which was a little difficult.
